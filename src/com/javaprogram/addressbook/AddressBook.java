fd3de85917a63103027cf8d1ae596f48bc89e6af
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-01 23:00:23 +0530

    Added Welcome Message

---
+++ src/com/javaprogram/addressbook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.javaprogram.addressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to address book program");
+	}
+
+}
