952cbc1351004e2f96fd90371d50194735fcc6ec
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-02 11:02:47 +0530

    Added code for creating contact details

---
+++ src/com/javaprogram/addressbook/InformationInput.java
@@ -0,0 +1,100 @@
+package com.javaprogram.addressbook;
+
+public class InformationInput {
+	
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String emailId;
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName=firstName;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setLastName(String lastName)
+	{
+		this.lastName=lastName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address=address;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setCity(String city)
+	{
+		this.city=city;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setState(String state)
+	{
+		this.state=state;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public void setZip(String zip)
+	{
+		this.zip=zip;
+	}
+
+	public String getZip()
+	{
+		return zip;
+	}
+
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber=phoneNumber;
+	}
+
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	public void setEmailId(String emailId)
+	{
+		this.emailId=emailId;
+	}
+
+	public String getEmail()
+	{
+		return emailId;
+	}
+	
+	public String toString() {
+		return "InformationInput [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phone=" + phoneNumber + ", emailId=" + emailId +"]";
+	}
+
+}
