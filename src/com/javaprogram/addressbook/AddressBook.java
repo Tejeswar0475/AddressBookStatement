9503b90243f29c4d3eb9935e25be137f3990c610
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-02 10:18:50 +0530

    Added code for creating a contacts in addressbook

--- src/com/javaprogram/addressbook/AddressBook.java
+++ src/com/javaprogram/addressbook/AddressBook.java
@@ -2,9 +2,100 @@ package com.javaprogram.addressbook;
 
 public class AddressBook {
 
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String emailId;
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName=firstName;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setLastName(String lastName)
+	{
+		this.lastName=lastName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address=address;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setCity(String city)
+	{
+		this.city=city;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setState(String state)
+	{
+		this.state=state;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public void setZip(String zip)
+	{
+		this.zip=zip;
+	}
+
+	public String getZip()
+	{
+		return zip;
+	}
+
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber=phoneNumber;
+	}
+
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	public void setEmailId(String emailId)
+	{
+		this.emailId=emailId;
+	}
+
+	public String getEmail()
+	{
+		return emailId;
+	}
+
 	public static void main(String[] args) {
-	
+
 		System.out.println("Welcome to address book program");
+
+
 	}
 
 }
